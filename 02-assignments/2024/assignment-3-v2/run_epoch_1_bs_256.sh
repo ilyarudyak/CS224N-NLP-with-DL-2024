98056e03127eb1e5d5966a899103a2ca8a27eaec
Ilia Rudiak <ilyarudyak@yahoo.com>
2026-08-31 21:30:48 +0200

    Prepare batch size increase to 256

---
+++ 02-assignments/2024/assignment-3-v2/run_epoch_1_bs_256.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+if [ "$1" = "train" ]; then
+	CUDA_VISIBLE_DEVICES=0 python run.py train \
+		--train-src=./zh_en_data/train.zh \
+		--train-tgt=./zh_en_data/train.en \
+		--dev-src=./zh_en_data/dev.zh \
+		--dev-tgt=./zh_en_data/dev.en \
+		--vocab=vocab.json \
+		--cuda \
+		--amp \
+		--pin-memory \
+		--num-workers=2 \
+		--lr=5e-4 \
+		--patience=3 \
+		--valid-niter=200 \
+		--batch-size=32 \
+		--embed-size=1024 \
+		--hidden-size=768 \
+		--dropout=.3 \
+		--max-epoch=1
+elif [ "$1" = "train_a100" ]; then
+	CUDA_VISIBLE_DEVICES=0 python run.py train \
+		--train-src=./zh_en_data/train.zh \
+		--train-tgt=./zh_en_data/train.en \
+		--dev-src=./zh_en_data/dev.zh \
+		--dev-tgt=./zh_en_data/dev.en \
+		--vocab=vocab.json \
+		--cuda \
+		--amp \
+		--pin-memory \
+		--num-workers=4 \
+		--lr=1.5e-3 \
+		--patience=3 \
+		--valid-niter=25 \
+		--batch-size=256 \
+		--embed-size=1024 \
+		--hidden-size=768 \
+		--dropout=.3 \
+		--max-epoch=1 \
+		--clip-grad=5.0
+elif [ "$1" = "train_mps" ]; then
+	python run.py train \
+		--train-src=./zh_en_data/train.zh \
+		--train-tgt=./zh_en_data/train.en \
+		--dev-src=./zh_en_data/dev.zh \
+		--dev-tgt=./zh_en_data/dev.en \
+		--vocab=vocab.json \
+		--mps \
+		--num-workers=0 \
+		--lr=5e-4 \
+		--patience=3 \
+		--valid-niter=200 \
+		--batch-size=32 \
+		--embed-size=1024 \
+		--hidden-size=768 \
+		--dropout=.3 \
+		--max-epoch=1
+elif [ "$1" = "train_toy_mps" ]; then
+	python run.py train \
+		--train-src=./zh_en_data/train.zh \
+		--train-tgt=./zh_en_data/train.en \
+		--dev-src=./zh_en_data/dev.zh \
+		--dev-tgt=./zh_en_data/dev.en \
+		--vocab=vocab.json \
+		--mps \
+		--num-workers=0 \
+		--max-samples=1000 \
+		--lr=5e-4 \
+		--patience=3 \
+		--valid-niter=50 \
+		--batch-size=32 \
+		--embed-size=256 \
+		--hidden-size=256 \
+		--dropout=.3 \
+		--max-epoch=1
+elif [ "$1" = "test" ]; then
+	CUDA_VISIBLE_DEVICES=0 python run.py decode model.bin ./zh_en_data/test.zh ./zh_en_data/test.en outputs/test_outputs.txt --cuda
+elif [ "$1" = "dev" ]; then
+	CUDA_VISIBLE_DEVICES=0 python run.py decode model.bin ./zh_en_data/dev.zh ./zh_en_data/dev.en outputs/dev_outputs.txt --cuda
+elif [ "$1" = "test_mps" ]; then
+	python run.py decode model.bin ./zh_en_data/test.zh ./zh_en_data/test.en outputs/test_outputs.txt --mps
+elif [ "$1" = "dev_mps" ]; then
+	python run.py decode model.bin ./zh_en_data/dev.zh ./zh_en_data/dev.en outputs/dev_outputs.txt --mps
+elif [ "$1" = "tensorboard" ]; then
+	tensorboard --logdir runs --bind_all
+else
+	echo "Invalid Option Selected. Valid options: train, train_a100, train_mps, train_toy_mps, test, dev, test_mps, dev_mps, tensorboard"
+fi
